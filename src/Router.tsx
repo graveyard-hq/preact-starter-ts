99e9b2c8c1a7717df1561ff43fd401f80c5e64b8
Aelpxy <aelpxy@protonmail.com>
2022-05-20 05:53:49 +0100

    feat: finished template

---
+++ src/Router.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* 404 Page */}
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+      <>
+      <section className="font-medium text-center text-black">
+      react-vite-tailwind-template
+      </section>
+      </>
+  );
+}
