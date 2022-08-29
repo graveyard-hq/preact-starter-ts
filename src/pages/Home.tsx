4876e2b493466ed689d51f3d71ed7d9455c2f092
Aelpxy <aelpxy@protonmail.com>
2022-08-29 17:52:05 +0000

    feat: same as nextjs page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-
 export default function Home() {
   return (
-      <>
-      <section className="font-mono text-center text-black">
-      npx degit aelpxy/react-tailwind-template
-      </section>
-      </>
+    <>
+      <div>
+        <h1 className="text-3xl text-center">
+          <span className="text-gray-900">React + Vite + </span>
+          <span className="text-blue-500">TypeScript + </span>
+          <span className="text-indigo-600">TailwindCSS</span>
+        </h1>
+        <div>
+          <div className="flex flex-col space-y-4 m-20">
+            <p className="text-lg text-center">Get started by</p>
+            <span className="font-mono text-center">
+              npx degit aelpxy/react-tailwind-template
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
