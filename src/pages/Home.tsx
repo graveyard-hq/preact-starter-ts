7bf37f08dca7a1035aa59d1f90e9b21f61931a41
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-09-11 16:12:19 +0600

    fix: remove `utm_source`

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
         <footer className={"footer"}>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://vercel.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
