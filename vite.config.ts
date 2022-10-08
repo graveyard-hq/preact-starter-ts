d4e9e90e041941b32aedef9095ea42beabf71aa7
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-10-08 13:48:48 +0100

    feat: separate vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+   build: {
+    outDir: 'build',
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
 })
